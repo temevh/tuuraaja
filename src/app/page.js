f56d2ca377a00acac4e0f3e92a84260ac09eea1c
temevh <temehama@gmail.com>
2024-10-30 14:36:50 +0200

    added find button

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,18 @@
 import SubjectDropdown from "./components/filters/SubjectDropdown";
 import CalendarComponent from "./components/filters/CalendarComponent";
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 export default function Home() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedSubject, setSelectedSubject] = useState("");
 
+  const fetchSubs = async () => {
+    console.log("finding subs...");
+  };
+
   return (
-    <div className="w-full bg-blue-400">
+    <div className="w-full bg-amber-600">
       <div className="w-1/2 grid grid-cols-1 gap-y-6">
         <SubjectDropdown
           selectedSubject={selectedSubject}
@@ -18,6 +23,7 @@ export default function Home() {
           selectedDate={selectedDate}
           onDateChange={setSelectedDate}
         />
+        <Button onClick={fetchSubs}>Find!</Button>
       </div>
     </div>
   );
