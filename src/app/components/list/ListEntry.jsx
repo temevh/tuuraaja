2d250494ac12ff92a519d8ea2a726e933a126768
temevh <temehama@gmail.com>
2024-11-01 09:32:13 +0200

    made the list entries a bit nicer

--- src/app/components/list/ListEntry.jsx
+++ src/app/components/list/ListEntry.jsx
@@ -1,7 +1,7 @@
 const ListEntry = ({ firstName, lastName, subjects, phoneNumber }) => {
   return (
-    <div className=" bg-red-400">
-      <p>
+    <div className=" bg-red-400 w-1/2 h-12 rounded-lg mt-2 flex items-center justify-center">
+      <p className="text-black text-xl text-center">
         {firstName}, {lastName}, {phoneNumber}
       </p>
     </div>
