2ce421bda1558c5ca85c290d6fa6ddede12bd76e
temevh <temehama@gmail.com>
2024-11-03 12:16:15 +0200

    added name, email and number fields and states to /mobile

--- src/app/mobile/page.jsx
+++ src/app/mobile/page.jsx
@@ -1,10 +1,10 @@
-import SubName from "../components/mobileFields/SubName";
+import SubInfo from "../components/mobileFields/SubInfo";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-amber-600 flex justify-center items-center p-6">
       <div className="grid grid-cols-1 gap-y-6">
-        <SubName />
+        <SubInfo />
       </div>
     </div>
   );
