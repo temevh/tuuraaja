5f98b062fdce7ed5e52d734654fc847d652e33b1
temevh <temehama@gmail.com>
2024-11-03 12:05:17 +0200

    started to work on the mobile view to add sub info

--- src/app/mobile/page.jsx
+++ src/app/mobile/page.jsx
@@ -1,8 +1,10 @@
+import SubName from "../components/mobileFields/SubName";
+
 export default function Page() {
   return (
     <div className="min-h-screen w-full bg-amber-600 flex justify-center items-center p-6">
-      <div className="w-1/2 grid grid-cols-1 gap-y-6">
-        <p>AAAA</p>
+      <div className="grid grid-cols-1 gap-y-6">
+        <SubName />
       </div>
     </div>
   );
