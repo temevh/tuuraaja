6e1a152b736f2b303fd44c6a6f5ebdaae4a31ab9
temevh <temehama@gmail.com>
2024-11-05 21:21:30 +0200

    switching to react-calendar instead of mui datecalendar

--- src/app/components/mobileFields/SubCalendar.jsx
+++ src/app/components/mobileFields/SubCalendar.jsx
@@ -1,17 +1,38 @@
 "use client";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { useState } from "react";
+import Calendar from "react-calendar";
+import "./SubCalendar.css";
 
 const SubCalendar = () => {
-  const [selectedDates, setSelectedDate] = useState([[]]);
-  const onDateClick = () => {};
+  const [value, setValue] = useState(new Date());
+
+  const highlightedDates = [
+    new Date(2024, 10, 5),
+    new Date(2024, 10, 10),
+    new Date(2024, 10, 15),
+  ];
+
+  const isHighlighted = (date) => {
+    return highlightedDates.some(
+      (highlightedDate) =>
+        date.getFullYear() === highlightedDate.getFullYear() &&
+        date.getMonth() === highlightedDate.getMonth() &&
+        date.getDate() === highlightedDate.getDate()
+    );
+  };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fi">
-      <DateCalendar displayWeekNumber />
-    </LocalizationProvider>
+    <div>
+      <Calendar
+        onChange={setValue}
+        value={value}
+        tileClassName={({ date, view }) => {
+          if (view === "month" && isHighlighted(date)) {
+            return "highlight";
+          }
+        }}
+      />
+    </div>
   );
 };
 
