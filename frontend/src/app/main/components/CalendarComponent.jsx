09e95c7ca3efaca013e5ae5349bfcba1eea0ae32
temevh <temehama@gmail.com>
2024-11-15 12:16:14 +0200

    calendar styling

--- frontend/src/app/main/components/CalendarComponent.jsx
+++ frontend/src/app/main/components/CalendarComponent.jsx
@@ -36,6 +36,9 @@ const CalendarComponent = ({ selectedDate, onDateChange }) => {
           }
         }}
         tileDisabled={({ date }) => [0, 6].includes(date.getDay())}
+        minDate={new Date()}
+        activeStartDate={new Date()}
+        showNeighboringCentury={false}
       />
     </CalendarContainer>
   );
