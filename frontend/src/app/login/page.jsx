1b4f75d77491e8baedc547db3ecf4cbadb6aefd8
temevh <temehama@gmail.com>
2024-11-18 21:57:18 +0200

    started to work on register page

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import axios from "axios";
+
+import { TextField, InputLabel } from "@mui/material";
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchSubjects = async () => {
+      console.log("loaded site");
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/getsubjects"
+        );
+        const tempSub = response.data;
+        const subjectNames = tempSub.map((subject) => subject.name);
+        setSubjects(subjectNames);
+        setLoading(false);
+      } catch (error) {
+        console.log("error loading courses", error);
+        setLoading(false);
+      }
+    };
+
+    fetchSubjects();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-6 bg-gray-600">
+      <div className="flex gap-6 items-center rounded-md">
+        {/*<p className="text-2xl text-">Etunimi</p>*/}
+        <TextField
+          label="Etunimi"
+          sx={{ width: 300, backgroundColor: "white" }}
+        ></TextField>
+        <TextField
+          label="Sukunimi"
+          sx={{ width: 300, backgroundColor: "white" }}
+        ></TextField>
+      </div>
+      <div className="flex gap-6 items-center rounded-md">
+        <TextField
+          label="Sähköposti"
+          sx={{ width: 300, backgroundColor: "white" }}
+        ></TextField>
+        <TextField
+          label="Puhelinnumero"
+          sx={{ width: 300, backgroundColor: "white" }}
+        ></TextField>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/signin/page.jsx
+++ frontend/src/app/signin/page.jsx
@@ -1,10 +1,18 @@
+"use client";
 import { Button } from "@mui/material";
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  const registerClicked = () => {
+    router.push("/login");
+  };
+
   return (
     <div className="flex items-center justify-center h-screen gap-24 bg-gray-600">
       <div className="bg-green-700 rounded-lg hover:bg-green-500">
-        <Button>
+        <Button onClick={registerClicked}>
           <p className="text-white font-bold text-xl hover:text-black">
             Kirjaudu sisään
           </p>
