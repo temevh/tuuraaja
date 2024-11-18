06eaeabc42c0f2d9404223c6d7703b4918130ffc
temevh <temehama@gmail.com>
2024-11-18 21:37:14 +0200

    added buttons for register and log in

--- frontend/src/app/signin/page.jsx
+++ frontend/src/app/signin/page.jsx
@@ -1,3 +1,22 @@
+import { Button } from "@mui/material";
+
 export default function Home() {
-  return <p>Sign in page</p>;
+  return (
+    <div className="flex items-center justify-center h-screen gap-24 bg-gray-600">
+      <div className="bg-green-700 rounded-lg hover:bg-green-500">
+        <Button>
+          <p className="text-white font-bold text-xl hover:text-black">
+            Kirjaudu sisään
+          </p>
+        </Button>
+      </div>
+      <div className="bg-green-700 rounded-lg hover:bg-green-500">
+        <Button>
+          <p className="text-white font-bold text-xl hover:text-black">
+            Luo profiili
+          </p>
+        </Button>
+      </div>
+    </div>
+  );
 }
