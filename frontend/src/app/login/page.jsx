4f01b90b14948c343e3af9c9440872c7aaa147f0
temevh <temehama@gmail.com>
2024-11-21 10:05:57 +0200

    started to work on login page (for existing user)

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,29 +1,41 @@
 "use client";
 import { Button } from "@mui/material";
-
 import { useRouter } from "next/navigation";
+
+import PasswordField from "../register/components/PasswordField";
+import { EmailField } from "../mobile/components";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const registerClicked = () => {
-    router.push("/register");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const updateEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const updatePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const checkCrendtials = () => {};
+
+  const loginClicked = () => {
+    router.push("/mobile");
   };
 
   return (
-    <div className="flex items-center justify-center h-screen gap-24 bg-gray-600">
-      <div className="bg-green-700 rounded-lg hover:bg-green-500">
-        <Button>
-          <p className="text-white font-bold text-xl hover:text-black">
-            Kirjaudu sisään
-          </p>
-        </Button>
-      </div>
-      <div className="bg-green-700 rounded-lg hover:bg-green-500">
-        <Button onClick={registerClicked}>
-          <p className="text-white font-bold text-xl hover:text-black">
-            Luo profiili
-          </p>
-        </Button>
+    <div className="flex items-center justify-center h-screen gap-24 bg-green-800">
+      <div className="flex flex-col gap-6">
+        <EmailField email={email} updateEmail={updateEmail} />
+        <PasswordField password={password} updatePassword={updatePassword} />
+        <button>
+          <div className="bg-green-600 rounded-lg">
+            <p className="text-black text-2xl font-bold">Kirjaudu sisään</p>
+          </div>
+        </button>
       </div>
     </div>
   );
