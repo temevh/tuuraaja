b58a8969971c3363d73db387d05fe5d5cd6bebca
temevh <temehama@gmail.com>
2024-11-29 12:39:02 +0200

    simplifying subinfo

--- frontend/src/app/mobile/[email]/page.jsx
+++ frontend/src/app/mobile/[email]/page.jsx
@@ -1,14 +1,32 @@
 "use client";
 import { useParams } from "next/navigation";
 import SubInfo from "../components/SubInfo";
+import axios from "axios";
+
+import FormatDates from "@/app/utils/functions/FormatDates";
 
 const userPage = () => {
   const { email } = useParams();
 
+  const updateDates = async (dates) => {
+    try {
+      const formattedDates = FormatDates(dates);
+      console.log("formattedDates", formattedDates);
+
+      const response = await axios.post("http://localhost:5000/api/adddates", {
+        email: email,
+        dates: formattedDates,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error adding dates", error);
+    }
+  };
+
   return (
     <div className="min-h-screen w-full bg-green-700 flex justify-center items-center p-6">
       <div className="grid grid-cols-1 gap-y-6">
-        <SubInfo />
+        <SubInfo updateDates={updateDates} />
         <p>Tervetuloa {email}</p>
       </div>
     </div>
--- frontend/src/app/mobile/components/SubInfo.jsx
+++ frontend/src/app/mobile/components/SubInfo.jsx
@@ -1,84 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
-import axios from "axios";
+import { useState } from "react";
 
-import {
-  EmailField,
-  FirstNameField,
-  LastNameField,
-  PhoneNumberField,
-  SubCalendar,
-  SubjectsField,
-} from "./index";
+import { SubCalendar } from "./index";
 
-const SubInfo = () => {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhonenumber] = useState(null);
-  const [subjects, setSubjects] = useState([]);
-  const [sent, setSent] = useState(false);
+const SubInfo = ({ updateDates }) => {
   const [selectedDates, setSelectedDates] = useState([]);
-  const [subjectList, setSubjectList] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchSubjects = async () => {
-      console.log("loaded site");
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/getsubjects"
-        );
-        const tempSub = response.data;
-        console.log("tempSub", tempSub);
-        const subjectNames = tempSub.map((subject) => subject.name);
-        setSubjectList(subjectNames);
-        console.log("subjects", subjectNames);
-        setLoading(false);
-      } catch (error) {
-        console.log("error loading courses", error);
-        setLoading(false);
-      }
-    };
-
-    fetchSubjects();
-  }, []);
-
-  /*
-  const updateFirstname = (event) => {
-    setFirstname(event.target.value);
-  };
-
-  const updateLastname = (event) => {
-    setLastname(event.target.value);
-  };
-
-  const updateEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const updateNumber = (event) => {
-    setPhonenumber(event.target.value);
-  };
-
-  const updateSubjects = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setSubjects(typeof value === "string" ? value.split(",") : value);
-  };
-  */
-
-  const resetForm = () => {
-    setFirstname("");
-    setLastname("");
-    setEmail("");
-    setPhonenumber(null);
-    setSubjects([]);
-    setSent(true);
-    setSelectedDates([]);
-  };
 
   const handleDateChange = (date) => {
     const dateString = date.toDateString();
@@ -95,86 +21,23 @@ const SubInfo = () => {
     } else {
       setSelectedDates((prevDates) => [...prevDates, date]);
     }
-  };
-
-  const formatDates = (selectedDates) => {
-    const pad = (number) => (number < 10 ? `0${number}` : number);
-
-    return selectedDates.map((date) => {
-      const year = date.getFullYear();
-      const month = pad(date.getMonth() + 1);
-      const day = pad(date.getDate());
-
-      return `${year}-${month}-${day}`;
-    });
-  };
-
-  const sendInfo = async () => {
-    try {
-      const formattedDates = formatDates(selectedDates);
-      console.log(formattedDates);
-      const response = await axios.post("http://localhost:5000/api/addsub", {
-        firstName: firstname,
-        lastName: lastname,
-        phoneNumber: phoneNumber,
-        email: email,
-        subjects: subjects,
-        dates: formattedDates,
-      });
-      console.log(response.data);
-      //resetForm();
-    } catch (error) {
-      console.error("Error sending info:", error);
-    }
+    console.log(selectedDates);
   };
 
   return (
     <div className="min-h-screen w-full bg-green-700 flex justify-center items-center p-6">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <div className="flex flex-col gap-6 pb-6">
-            <p className="text-5xl">Valitse sopivat päivämäärät</p>
-            {/*
-            <FirstNameField
-              firstName={firstname}
-              updateFirstName={updateFirstname}
-            />
-            <LastNameField
-              lastName={lastname}
-              updateLastName={updateLastname}
-            />
-          </div>
-          <div className="flex gap-6">
-            <EmailField email={email} updateEmail={updateEmail} />
-            <PhoneNumberField
-              phoneNumber={phoneNumber}
-              updateNumber={updateNumber}
-            />
-          </div>
-          <SubjectsField
-            subjectList={subjectList}
-            selectedSubjects={subjects}
-            updateSelectedSubjects={updateSubjects}
+      <div>
+        <div className="flex flex-col gap-6 pb-6">
+          <p className="text-5xl">Valitse sopivat päivämäärät</p>
+          <SubCalendar
+            onDateChange={handleDateChange}
+            selectedDates={selectedDates}
           />
-
-          <div className="flex pb-6">
-            */}
-            <SubCalendar
-              onDateChange={handleDateChange}
-              selectedDates={selectedDates}
-            />
-          </div>
-          <div className="bg-purple-500 mt-8 text-center">
-            <Button onClick={sendInfo}>
-              <p className="text-xl text-black">
-                {sent ? "Tallennettu" : "Tallenna"}
-              </p>
-            </Button>
-          </div>
         </div>
-      )}
+        <div className="bg-purple-500 mt-8 text-center">
+          <button onClick={() => updateDates(selectedDates)}>Press me</button>
+        </div>
+      </div>
     </div>
   );
 };
