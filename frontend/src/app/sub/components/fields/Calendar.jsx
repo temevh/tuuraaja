9854ffa17ed961737c3f8e3b6293b495f8192048
temevh <temehama@gmail.com>
2024-12-08 20:08:54 +0200

    calendar styling

--- frontend/src/app/sub/components/fields/Calendar.jsx
+++ frontend/src/app/sub/components/fields/Calendar.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { DayPicker } from "react-day-picker";
+import { DayPicker, getDefaultClassNames } from "react-day-picker";
 import "react-day-picker/style.css";
 
 const Calendar = ({ selectedDates }) => {
+  const defaultClassNames = getDefaultClassNames();
   const [selected, setSelected] = useState([]);
 
   useEffect(() => {
@@ -13,7 +14,17 @@ const Calendar = ({ selectedDates }) => {
   return (
     <div>
       <p className="text-3xl">Valitse sopivat päivämäärät</p>
-      <DayPicker mode="multiple" selected={selected} onSelect={setSelected} />
+      <DayPicker
+        mode="multiple"
+        selected={selected}
+        onSelect={setSelected}
+        classNames={{
+          today: `border-amber-300`,
+          selected: `bg-amber-500 border-amber-500 text-white`,
+          root: `${defaultClassNames.root} shadow-lg p-5`,
+          chevron: `${defaultClassNames.chevron} fill-green-500`,
+        }}
+      />
     </div>
   );
 };
