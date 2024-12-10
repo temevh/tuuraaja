a0fced1168b7093a89c62cb432fdca0599ea2336
temevh <temehama@gmail.com>
2024-12-10 22:30:08 +0200

    first version of postcard

--- frontend/src/app/main/components/PostCard.jsx
+++ frontend/src/app/main/components/PostCard.jsx
@@ -1,8 +1,31 @@
 const PostCard = ({ post }) => {
+  const date = new Date(post.date);
+  const formattedDate = `${date.getDate().toString().padStart(2, "0")}.${(
+    date.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}.${date.getFullYear()}`;
+
+  const secondarySubs = post.secondarySubs;
+
   return (
-    <div className="bg-red-300 rounded-md">
-      <p>{post.date}</p>
-      <p>{post.subject}</p>
+    <div className="bg-green-300 rounded-md pl-2 flex flex-cols-3">
+      <div>
+        <p className="text-xl font-bold text-black">{post.subject}</p>
+        <p className="text-lg text-black">{formattedDate}</p>
+      </div>
+      <div className="pl-6">
+        <div className="flex flex-rowss">
+          <p className="text-black text-xl pr-2">Ensisijainen:</p>
+          <p className="text-black text-xl underline">{post.primarySub}</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <p className="text-black">Varalla:</p>
+          {secondarySubs.map((sub) => {
+            return <p className="text-gray-600">{sub}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
