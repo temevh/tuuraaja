3e213b431481f6ff3374a8b6f1ce5a29241e36d9
temevh <temehama@gmail.com>
2024-12-11 11:33:53 +0200

    renamed /report to /post

--- frontend/src/app/post/[code]/page.jsx
+++ frontend/src/app/post/[code]/page.jsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useParams } from "next/navigation";
+
 const ReportPage = () => {
   //The pages route should be randomly generated
+  const params = useParams();
+  const postCode = params.code;
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <p className="text-2xl font-bold">REPORT PAGE</p>
+      <p className="text-2xl font-bold">REPORT PAGE {postCode}</p>
     </div>
   );
 };
