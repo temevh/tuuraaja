a5eae1a572297e8977bf5c74c1f2590c90d38940
temevh <temehama@gmail.com>
2025-01-13 17:11:06 +0200

    misc stuff

--- frontend/src/app/sub/components/SubPostCard.jsx
+++ frontend/src/app/sub/components/SubPostCard.jsx
@@ -1,4 +1,8 @@
+import { useRouter } from "next/navigation";
+
 const SubPostCard = ({ post }) => {
+  const router = useRouter();
+
   const date = new Date(post[0].date);
   const formattedDate = `${date.getDate().toString().padStart(2, "0")}.${(
     date.getMonth() + 1
@@ -6,8 +10,15 @@ const SubPostCard = ({ post }) => {
     .toString()
     .padStart(2, "0")}.${date.getFullYear()}`;
 
+  const postClicked = () => {
+    console.log(post);
+  };
+
   return (
-    <div className="bg-green-300 rounded-md pl-2 flex flex-cols-3 mb-4 w-full p-2">
+    <div
+      className="bg-green-300 rounded-md pl-2 flex flex-cols-3 mb-4 w-full p-2"
+      onClick={postClicked}
+    >
       <div>
         <div className="flex flex-row gap-2">
           <p className="text-xl font-bold text-black">{post[0].subject}</p>
